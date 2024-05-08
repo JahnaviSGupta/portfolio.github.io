050b3180a28a8f04ec28662fbf8be84843f17b42
Jahnavi <jahnavisgupta@gmail.com>
2024-05-07 21:00:30 -0400

    Changes in the Lets connect page(email configuration)

--- contact_me.js
+++ contact_me.js
@@ -6,12 +6,12 @@ function sendMail(){
     message : document.getElementById("demo-message").value,
     }
 
-    if (name === "" || email === "" || message === "") {
-        alert("Please fill in all the fields.");
-        return; // Exit the function if any field is empty
-    }
+    // if (name === "" || email === "" || message === "") {
+    //     alert("Please fill in all the fields.");
+    //     return; // Exit the function if any field is empty
+    // }
 
-    else{
+    
 emailjs.send("service_8fnt02n","template_xnnft4t",parms).then(alert("Email Sent!"))
-    }
+    
 }
